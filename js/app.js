cd20e3bf631a0d2f28c827f53eb8874fbfcc84c0
Guilherme Damas Da Silveira <140725276+Silveira1808@users.noreply.github.com>
2024-05-19 22:50:20 -0300

    Amigo secreto
    Segundo Desafio

--- js/app.js
+++ js/app.js
@@ -1,16 +1,57 @@
-function alterarStatus(id) {
-    let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('.dashboard__item__img');
-    let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+let amigos = [];
 
-    if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
-    } else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver';
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '') {
+        alert('Informe o nome do amigo!');
+        return;
     }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
+    let lista = document.getElementById('lista-amigos');
+
+    amigos.push(amigo.value);
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+
+    amigo.value = '';
+}
+
+function sortear() {
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+
+    embaralhar(amigos);
+
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+        }
+    }
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
